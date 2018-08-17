c3639bc9e94cf379704fc61511c231e7bea8a3f2
HUSTGS <1148690954@qq.com>
2018-08-18 00:08:33 +0800

    basically finish ot

--- ot.py
+++ ot.py
@@ -1,9 +1,29 @@
 # 收到的是基于某一个版本的changeset数组即ops
 #op.version = number
-# op.changeset = [step1, step2, step3...]
+# op.changeset = [actionA, actionB, step3...]
 # step.type = 'insert' / 'delete'
 # step.content = content
 # step.index = number
+
+from functools import reduce
+
+
+def follow(actionA, actionB):
+    if actionA == None or actionB == None:
+        return actionB
+    if actionA.type == actionB.type == 'insert':
+        return Step('insert', actionB.content, actionB.index if actionA.index > actionB.index else actionB.index + 1)
+    elif actionA.type == 'insert' and actionB.type == 'delete':
+        return Step('delete', None, actionB.index + 1 if actionA.index < actionB.index else actionB.index)
+    elif actionA.type == 'delete' and actionB.type == 'insert':
+        return Step('insert', actionB.content, actionB.index if actionA.index > actionB.index else actionB.index - 1)
+    elif actionA.type == actionB.type == 'delete':
+        if actionA.index > actionB.index:
+            return Step('delete', None, actionB.index)
+        elif actionB.index < actionB.index:
+            return Step('delete', None, actionB.index - 1)
+        else:
+            return None
 
 
 class Step(object):
@@ -22,19 +42,28 @@ class CooperationDocument(object):
     def __init__(self, ops=[]):
         self.ops = ops[:]
 
-    def follow(self, step1, step2):
-        if step1 == None or step2 == None:
-            return step2
-        if step1.type == step2.type == 'insert':
-            return Step('insert', step2.content, step2.index if step1.index > step2.index else step2.index + 1)
-        elif step1.type == 'insert' and step2.type == 'delete':
-            return Step('delete', None, step2.index + 1 if step1.index < step2.index else step2.index)
-        elif step1.type == 'delete' and step2.type == 'insert':
-            return Step('insert', step2.content, step2.index if step1.index > step2.index else step2.index - 1)
-        elif step1.type == step2.type == 'delete':
-            if step1.index > step2.index:
-                return Step('delete', None, step2.index)
-            elif step2.index < step2.index:
-                return Step('delete', None, step2.index - 1)
-            else:
-                return None
+    @staticmethod
+    def ot(changesetA, changesetB):
+        A_prime = []
+        temp = changesetA[:]
+        for action in changesetB.actions:
+            temp.insert(0, action)
+            result = reduce(lambda actionA, actionB: follow(
+                actionB, actionA), temp)
+            A_prime.append(result)
+            temp = map(lambda action_in_B: follow(
+                action_in_B, action), changesetA)
+        return A_prime
+
+
+class Action():
+    def __init__(self, action):
+        self.type = action['type']
+        self.content = action['content']
+        self.index = action['index']
+
+
+class Changeset():
+    def __init__(self, changeset, version):
+        self.actions = changeset.actions[:]
+        self.version = version
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, jsonify
-from json import loads
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+import mysql.connector
+import datetime
+import json
 app = Flask(__name__)
 
-docversion = 0
+con = mysql.connector.connect(
+    user='root', password='MySQL#232', database='CooperationEdit', port=3305, use_unicode=True
+)
+cursor = con.cursor()
 
 
 class Step(object):
@@ -13,12 +18,19 @@ class Step(object):
 
 
 class Op(object):
-    def __init__(self, changeset):
+    def __init__(self, changeset, version):
         self.changeset = changeset[:]
+        self.version = version
 
 
-def applychangeset(op):
-    pass
+def applychangeset(actions, doc):
+    for action in actions:
+        if action['type'] == 'insert':
+            doc = doc[:action['index']] + \
+                action['content'] + doc[action['index']:]
+        elif action['type'] == 'delete':
+            doc = doc[:action['index']-1] + doc[action['index']:]
+    return doc
 
 
 ''' def follow(step1, step2):
@@ -56,28 +68,43 @@ def follow(step1, step2):
         else:
             return None
 
+# table docs 顺序 username, docname, version, changeset, doc, date
+
 
 @app.route('/', methods=['GET'])
 def login():
     return render_template('editepage.html')
 
 
-@app.route('/merge', methods=['GET', 'POST'])
-def edite():
-    # print(request.get_data().decode())
-    # return 'xs'
-    temp = loads(request.get_data().decode())
-    op = Op(temp['actions'])
-    op.version = temp['localversion']
-    if op.version == docversion:
-        applychangeset(op)
-    elif op.version < docversion:
-        pass
-    print(type(temp), type(temp['actions']))
+# 请求版本号
+@app.route('/<username>/<docname>/requestversion', methods=['GET', 'POST'])
+def rqversion(username, docname):
+    print('username = {}, docname = {}'.format(username, docname))
+    cursor.execute(
+        "select * from docs where username = %s and docname = %s", (username, docname))
+    result = cursor.fetchall()
+    print(result)
+    return '0' if result == [] else str(result[0][2])
 
-    for step in temp['actions']:
-        print(step['type'], step['content'], step['index'])
-    return 'cds'
+
+@app.route('/<username>/<docname>/merge', methods=['GET', 'POST'])
+def merge(username, docname):
+    #str_changeset = request.get_data().decode()
+    changeset = json.loads(request.get_data().decode())
+    cursor.execute(
+        "select * from docs where username = %s and docname = %s and version = (select max(version) from docs where username = %s and docname = %s)", (username, docname, username, docname))
+    result = cursor.fetchall()
+    curent_version = result[0][2]
+    doc = result[0][4]
+    print(changeset)
+    if changeset.localversion == curent_version:
+        newdoc = applychangeset(changeset.actions, doc)
+        cursor.execute("insert into docs (username, docname, version, changeset, doc, date) values (%s, %s, %s, %s, %s, %s)",
+                       (username, docname, curent_version+1, str(changeset), newdoc, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        con.commit()
+        print(newdoc)
+    # elif changeset.localversion
+    return '123'
 
 
 if __name__ == '__main__':
